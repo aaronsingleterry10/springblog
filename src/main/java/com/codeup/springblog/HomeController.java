75fd4af0f08a80a9543938996ac7b4fafd601f29
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-31 14:36:25 -0500

    added code

--- src/main/java/com/codeup/springblog/HomeController.java
+++ src/main/java/com/codeup/springblog/HomeController.java
@@ -1,7 +1,10 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,6 +13,12 @@ public class HomeController {
     @GetMapping("/")
 //    @ResponseBody
     public String landingPage() {
+        return "home";
+    }
+
+    @PostMapping("/")
+    public String returnCohort(@RequestParam(name = "cohort") String cohort, Model model) {
+        model.addAttribute("cohort", cohort);
         return "home";
     }
 }
