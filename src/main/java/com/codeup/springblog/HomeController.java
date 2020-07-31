0a995f69cc91a44e74d182da14c16920cec1a127
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-31 14:04:38 -0500

    working on lecture

--- src/main/java/com/codeup/springblog/HomeController.java
+++ src/main/java/com/codeup/springblog/HomeController.java
@@ -8,8 +8,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody
+//    @ResponseBody
     public String landingPage() {
-        return "This is the landing page!";
+        return "home";
     }
 }
