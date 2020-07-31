baee370b58ab260f422f8a5d2f8f1dd8c686d19f
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-31 11:58:46 -0500

    added several arithmetic problems for GetMapping

--- src/main/java/com/codeup/springblog/MathController.java
+++ src/main/java/com/codeup/springblog/MathController.java
@@ -1,4 +1,35 @@
 package com.codeup.springblog;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class MathController {
+
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public String addition(@PathVariable int num1, @PathVariable int num2) {
+        return "" + (num1 + num2);
+    }
+
+    @GetMapping("subtract/{num1}/and/{num2}")
+    @ResponseBody
+    public String subtraction(@PathVariable int num1, @PathVariable int num2) {
+        int newInt = num1 - num2;
+        return Math.abs(newInt) + "";
+    }
+
+    @GetMapping("multiply/{num1}/and/{num2}")
+    @ResponseBody
+    public String multiplication(@PathVariable int num1, @PathVariable int num2) {
+        return num1 * num2 + "";
+    }
+
+    @GetMapping("divide/{num1}/and/{num2}")
+    @ResponseBody
+    public String division(@PathVariable int num1, @PathVariable int num2) {
+        return num1 / num2 + "";
+    }
 }
