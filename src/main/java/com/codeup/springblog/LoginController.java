68d1c46bbc83894d79bc65dbac366892de1d3323
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-31 16:13:04 -0500

    pushing up

--- src/main/java/com/codeup/springblog/LoginController.java
+++ src/main/java/com/codeup/springblog/LoginController.java
@@ -1,4 +1,21 @@
 package com.codeup.springblog;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class LoginController {
+
+    @GetMapping("/login")
+    public String landingPage() {
+        return "login";
+    }
+
+
 }
--- src/main/java/com/codeup/springblog/ProfileController.java
+++ src/main/java/com/codeup/springblog/ProfileController.java
@@ -4,13 +4,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ProfileController {
 
-    @GetMapping("/profile/{username}")
-    public String goToProfile(@PathVariable String username, Model model) {
-        model.addAttribute("user", username);
+    @GetMapping("/profile")
+    public String goToProfile() {
+        return "profile";
+    }
+
+    @PostMapping("/profile")
+    public String returnUsername(@RequestParam(name = "username") String username, @RequestParam(name = "password") String password, Model model) {
+        List<String> ads = new ArrayList<>();
+        ads.add("add 1");
+        ads.add("add 2");
+        ads.add("add 3");
+
+        model.addAttribute("username", username);
+        model.addAttribute("password", password);
+        model.addAttribute("ads", ads);
         return "profile";
     }
 }
