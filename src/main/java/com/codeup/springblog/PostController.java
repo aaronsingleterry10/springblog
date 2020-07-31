08aacf5c65ca8fb2cfbf0a17d2c7d496a69301c8
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-31 13:45:21 -0500

    completed exercise

--- src/main/java/com/codeup/springblog/PostController.java
+++ src/main/java/com/codeup/springblog/PostController.java
@@ -1,4 +1,35 @@
 package com.codeup.springblog;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class PostController {
+
+    @GetMapping("/posts")
+    @ResponseBody
+    public String getPosts() {
+        return "This is where posts will be posted";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String getIndividualPost(@PathVariable int id) {
+        return "This is where an individual post with an id of " + id + " will be.";
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String createPost() {
+        return "This is where posts are created";
+    }
+
+    @PostMapping("/posts/create")
+    @ResponseBody
+    public String newPost() {
+        return "This is a new post create";
+    }
 }
