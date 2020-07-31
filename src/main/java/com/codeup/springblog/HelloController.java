00a9444ebf37e7ad75b679aeb5c3299cf519fe40
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-07-31 11:21:19 -0500

    added to lecture

--- src/main/java/com/codeup/springblog/HelloController.java
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -1,8 +1,7 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloController {
@@ -17,5 +16,23 @@ public class HelloController {
     @ResponseBody
     public String goodbye() {
         return "Goodbye from Spring!";
+    }
+
+//    @GetMapping("/hello/{name}")
+//    @ResponseBody
+//    public String sayHello(@PathVariable String name) {
+//        return "Hello, " + name;
+//    }
+
+    @GetMapping("/books/{id}")
+    @ResponseBody
+    public String getBook(@PathVariable long id) {
+        return "Viewing book " + id;
+    }
+
+    @RequestMapping(path = "/hello/{name}", method = RequestMethod.GET)
+    @ResponseBody
+    public String sayHello(@PathVariable String name) {
+        return "Hello " + name + " from a request a mapping!";
     }
 }
