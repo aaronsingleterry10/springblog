e053c314f006595a185470e40147df119735e267
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-08-01 11:10:25 -0500

    completed first part of exercise

--- src/main/java/com/codeup/springblog/RollDiceController.java
+++ src/main/java/com/codeup/springblog/RollDiceController.java
@@ -1,13 +1,26 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class RollDiceController {
 
     @GetMapping("/roll-dice")
     public String diceRoll() {
-        return "";
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String guessedNumber(@PathVariable int n, Model model) {
+        int num = 4;
+        if (n == num) {
+            model.addAttribute("guessedNumber", "Congratulations! " + n + " is the correct number!");
+        } else {
+            model.addAttribute("guessedNumber", "Sorry, " + n + " is not the correct number. Try again!");
+        }
+        return "roll-dice";
     }
 }
