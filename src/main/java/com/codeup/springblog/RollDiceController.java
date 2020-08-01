9e3e06c073659d0a06f4d27b5339d44e528ddaef
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-08-01 11:40:38 -0500

    refactored RollDiceController to implement a random number generator

--- src/main/java/com/codeup/springblog/RollDiceController.java
+++ src/main/java/com/codeup/springblog/RollDiceController.java
@@ -1,9 +1,10 @@
 package com.codeup.springblog;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Random;
 
 @Controller
 public class RollDiceController {
@@ -15,11 +16,13 @@ public class RollDiceController {
 
     @GetMapping("/roll-dice/{n}")
     public String guessedNumber(@PathVariable int n, Model model) {
-        int num = 4;
+        Random rand = new Random();
+        int num = rand.nextInt(6) + 1;
+        model.addAttribute("rolledNum", "The number rolled is: " + num);
         if (n == num) {
-            model.addAttribute("guessedNumber", "Congratulations! " + n + " is the correct number!");
+            model.addAttribute("guessedNum", "Congratulations! " + n + " is the correct number!");
         } else {
-            model.addAttribute("guessedNumber", "Sorry, " + n + " is not the correct number. Try again!");
+            model.addAttribute("guessedNum", "Sorry, " + n + " is not the correct number. Try again!");
         }
         return "roll-dice";
     }
