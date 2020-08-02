89460d3f1eaa219712e07e0fbda66e0ed0b196d0
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-08-02 16:09:01 -0500

    completed bonus

--- src/main/java/com/codeup/springblog/RollDiceController.java
+++ src/main/java/com/codeup/springblog/RollDiceController.java
@@ -4,6 +4,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @Controller
@@ -16,13 +18,34 @@ public class RollDiceController {
 
     @GetMapping("/roll-dice/{n}")
     public String guessedNumber(@PathVariable int n, Model model) {
+        List<Integer> dice = new ArrayList<>();
         Random rand = new Random();
+        Random rand2 = new Random();
+        Random rand3 = new Random();
         int num = rand.nextInt(6) + 1;
-        model.addAttribute("rolledNum", "The number rolled is: " + num);
-        if (n == num) {
-            model.addAttribute("guessedNum", "Congratulations! " + n + " is the correct number!");
+        int num2 = rand2.nextInt(6) + 1;
+        int num3 = rand3.nextInt(6) + 1;
+        dice.add(num);
+        dice.add(num2);
+        dice.add(num3);
+
+        model.addAttribute("rolledNum", "Your lucky numbers are: " + num + ", " + num2 + ", " + num3 + ".");
+        model.addAttribute("userNum", "The number you have chosen is: " + n + ".");
+        int howManyMatches = 0;
+        for(Integer x : dice) {
+            if(n == x) {
+                howManyMatches += 1;
+            }
+        }
+
+        if (howManyMatches == 1) {
+            model.addAttribute("guessedNum", "You have " + howManyMatches + " matches");
+        } else if(howManyMatches == 2) {
+            model.addAttribute("guessedNum", "You have " + howManyMatches + " matches");
+        } else if(howManyMatches == 3) {
+            model.addAttribute("guessedNum", "You have " + howManyMatches + " matches");
         } else {
-            model.addAttribute("guessedNum", "Sorry, " + n + " is not the correct number. Try again!");
+            model.addAttribute("guessedNum", "You have " + howManyMatches + " matches");
         }
         return "roll-dice";
     }
