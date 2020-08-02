b106559118ab819c30d14c2c7e8aed5de5623de2
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-08-02 18:28:13 -0500

    added private int id. refactored constructor and added getter/setter

--- src/main/java/com/codeup/springblog/Post.java
+++ src/main/java/com/codeup/springblog/Post.java
@@ -1,12 +1,21 @@
 package com.codeup.springblog;
 
 public class Post {
+    private int id;
     private String title;
     private String body;
 
-    public Post(String title, String body) {
+    public Post(int id, String title, String body) {
+        this.id = id;
         this.title = title;
         this.body = body;
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getTitle() {
