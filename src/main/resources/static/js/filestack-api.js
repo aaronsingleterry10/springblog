afba1f96657b00d5e76c709fdcbd62c65de3c002
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-08-16 19:21:16 -0500

    added filestack api to js file

--- src/main/resources/static/js/filestack-api.js
+++ src/main/resources/static/js/filestack-api.js
@@ -1,4 +1,14 @@
-
-
-console.log("hello world");
+(function () {
+    const client = filestack.init(FILESTACK_KEY);
+    document.getElementById("upload").addEventListener("click", (e) => {
+        e.preventDefault();
+        const options = {
+            imageMax: [225, 225],
+            onFileUploadFinished: res => {
+                $("#img").attr("value", res.url);
+            }
+        };
+        client.picker(options).open();
+    });
+})();
 
