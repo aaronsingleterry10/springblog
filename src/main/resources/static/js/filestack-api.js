2e5cbef4916168a818c18db2609d8c72cf78e3e5
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-08-19 18:08:58 -0500

    refactored some code

--- src/main/resources/static/js/filestack-api.js
+++ src/main/resources/static/js/filestack-api.js
@@ -6,19 +6,33 @@
         e.preventDefault();
         const options = {
             imageMax: [400, 400],
-            // onFileUploadFinished: res => {
-            //     console.log(res.originalFile.type);
-            //     $("#img").attr("value", res.url);
-            //     $("#file-type").attr("value", res.originalFile.type)
-            // }
             maxFiles: 4,
             onUploadDone: res => {
                 for (let i = 0; i < res.filesUploaded.length; i++) {
                     urlArray.push(res.filesUploaded[i].url);
                     fileTypeArray.push(res.filesUploaded[i].originalFile.type);
                 }
+                let photosHtml = "";
+                let videosHtml = "";
+                for (let i = 0; i < urlArray.length; i++) {
+                    if (fileTypeArray[i] === "image/jpeg") {
+                        photosHtml += `
+                        <img src="${urlArray[i]}" alt="" class="mb-3">
+                        <br>
+                        `;
+                    } else if (fileTypeArray[i] === "video/mp4") {
+                        videosHtml += `
+                        <video width="400" height="400" controls>
+                            <source src="${urlArray[i]}">
+                        </video>
+                        <br>
+                        `;
+                    }
+                }
+                let photoAndVideos = photosHtml + videosHtml;
                 $("#images").val(urlArray);
                 $("#file-type").val(fileTypeArray);
+                $("#files").html("<h3>You're uploaded files:</h3><br>" + photoAndVideos);
             }
         };
         client.picker(options).open();
