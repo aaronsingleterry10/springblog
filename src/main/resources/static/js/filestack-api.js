b10baccc1d2241cd2d390c60987910de19ff3473
Aaron Singleterry <aaronsingleterry10@gmail.com>
2020-08-19 16:32:54 -0500

    refactored filestack api implementation

--- src/main/resources/static/js/filestack-api.js
+++ src/main/resources/static/js/filestack-api.js
@@ -1,11 +1,24 @@
 (function () {
     const client = filestack.init(FILESTACK_KEY);
+    let urlArray = [];
+    let fileTypeArray = [];
     document.getElementById("upload").addEventListener("click", (e) => {
         e.preventDefault();
         const options = {
-            imageMax: [225, 225],
-            onFileUploadFinished: res => {
-                $("#img").attr("value", res.url);
+            imageMax: [400, 400],
+            // onFileUploadFinished: res => {
+            //     console.log(res.originalFile.type);
+            //     $("#img").attr("value", res.url);
+            //     $("#file-type").attr("value", res.originalFile.type)
+            // }
+            maxFiles: 4,
+            onUploadDone: res => {
+                for (let i = 0; i < res.filesUploaded.length; i++) {
+                    urlArray.push(res.filesUploaded[i].url);
+                    fileTypeArray.push(res.filesUploaded[i].originalFile.type);
+                }
+                $("#images").val(urlArray);
+                $("#file-type").val(fileTypeArray);
             }
         };
         client.picker(options).open();
